0f9574e05dab657515521a36ebe154b39db8f269
kouh <kouh28@gmail.com>
2014-03-29 15:13:54 +0900

    Add 01_hello-world

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-  echo "php practice";
-
-?>
+<!doctype html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP-Practice</title>
+</head>
+<body>
+  <ul>
+    <li><a href="01_hello-world.php">Hello World!</a></li>
+  </ul>
+</body>
+</html>
